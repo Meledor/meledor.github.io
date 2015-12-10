7f02e9c2ad00cd63757b7a9e036fd9a77f4f78aa
Meledor <michaelzimmermann1@hotmail.com>
2015-12-10 18:37:06 +0100

    läuft

--- js/base.js
+++ js/base.js
@@ -16,3 +16,40 @@ function validateForm(form) {
     } 
     return success;
 }
+
+$(document).ready(function() { 
+function changeStyles(optValue){ 
+var property = optValue.split("-")[0]; 
+removeOldClass(property); 
+$("body").addClass(optValue); 
+$.cookie('bodyClassList', $("body").attr('class')); 
+}	         
+function removeOldClass(property){ 
+var classList = $("body").attr('class').split(/\s+/); 
+$.each(classList, function(index, item){ 
+if (item.match(property)){ 
+$("body").removeClass(item); 
+} 
+}); 
+} 
+$(".selector").change(function(){ 
+changeStyles(this.value); 
+}); 
+      
+$(".selector > option").each(function() { 
+if($.cookie('bodyClassList') != null && $.cookie('bodyClassList').match($(this).val())){ 
+$(this).attr("selected","selected");  
+} 
+}); 
+$("#reset").click(function(){ 
+$("body").removeClass(); 
+$(".selector").each(function(){ 
+$(this[0]).attr('selected', true); 
+}) 
+ $.cookie('bodyClassList', null); 
+});  
+if($.cookie('bodyClassList') != null) {  
+$("body").removeClass(); 
+$("body").addClass($.cookie('bodyClassList')); 
+} 
+}); 
